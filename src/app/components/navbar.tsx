bb456631e6ec878b9ef83a9eb07a6363b00e6680
Manikya Shama <253427819+manikya-ua@users.noreply.github.com>
2026-01-27 11:09:50 +0530

    add tooltip

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -6,15 +6,17 @@ import WithTooltip from "./with-tooltip";
 export default function Navbar() {
   return (
     <nav className="bg-inherit h-14 border-b border-neutral-700 text-neutral-200 flex items-center px-4 isolate">
-      <Link href="/">
-        <Image
-          src="/logo.svg"
-          width={76}
-          height={22}
-          alt="Reddit Logo"
-          className="text-white"
-        />
-      </Link>
+      <WithTooltip tooltipText="Go to Reddit Home" side="right">
+        <Link href="/">
+          <Image
+            src="/logo.svg"
+            width={76}
+            height={22}
+            alt="Reddit Logo"
+            className="text-white"
+          />
+        </Link>
+      </WithTooltip>
       <div role="presentation" className="grow shrink"></div>
       <SearchBar />
       <div role="presentation" className="grow shrink"></div>
--- src/app/components/with-tooltip.tsx
+++ src/app/components/with-tooltip.tsx
@@ -1,12 +1,22 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
 export default function WithTooltip({
   children,
   tooltipText,
+  side,
 }: {
   children: React.ReactNode;
   tooltipText: string;
+  side?: "left" | "top" | "bottom" | "right";
 }) {
   return (
-    // TODO: Add tooltip functionality
-    <div data-tooltip={tooltipText}>{children}</div>
+    <Tooltip>
+      <TooltipTrigger>{children}</TooltipTrigger>
+      <TooltipContent side={side}>{tooltipText}</TooltipContent>
+    </Tooltip>
   );
 }
